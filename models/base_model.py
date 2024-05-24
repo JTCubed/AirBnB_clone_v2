0a72f30256da8a0a55c9bb11c5930ec81c9bc201
JTCubed <zeusio60@gmail.com>
2024-05-24 03:33:12 +0200

    Revert "updated files"
    This reverts commit da24c15e342c68ccadcd82e63bc524285d5b523c.
    
    revert git commit

--- models/base_model.py
+++ models/base_model.py
@@ -2,45 +2,23 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, create_engine, DateTime
-from sqlalchemy.orm import declarative_base
 
-
-Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-#            from models import storage
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            print(kwargs)
-#            storage.new(self)
+            storage.new(self)
         else:
-            for key, value in kwargs.items():
-                if key == 'updated_at' or key == 'created_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if not hasattr(self, key):
-                    setattr(self, key, value)
-            print(kwargs)
-            print(self)
-#            storage.new(self)
-#                    setattr(self, key, datetime.strptime(
-#                        value, '%Y-%m-%dT%H:%M:%S.%f'))
-#                elif key != '__class__' and key != '_sa_instance_state':
-#                    setattr(self, key, value)
-#            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-#                                                     '%Y-%m-%dT%H:%M:%S.%f')
-#            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-#                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -53,7 +31,6 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -64,11 +41,4 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if hasattr(dictionary, "_sa_instance_state"):
-            dictionary.pop("_sa_instance_state")
         return dictionary
-
-    def delete(self):
-        """deletes current obj"""
-        from models import storage
-        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
-from sqlalchemy.orm import relationship, backref
-import os
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
-        @property
-        def cities(self):
-            """
-            Returns the list of City instances with state_id equals to
-            the current State.id
-            """
-            from model import storage
-            cities_l = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    cities_l.append(city)
-            return cities_l
+    name = ""
